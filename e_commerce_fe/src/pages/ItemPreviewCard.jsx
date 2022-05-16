3f92b20fa6d5b5f9bad0618f97438c16c92ea3c3
MahendraThammita <dhmmpthammita@gmail.com>
2022-05-17 03:37:53 +0530

    Payment UI Sketches completed

---
+++ e_commerce_fe/src/pages/ItemPreviewCard.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Layout, Row, Col, Image, Button, Typography  } from 'antd';
+const { Title, Text } = Typography;
+
+export default class ItemPreviewCard extends Component {
+  render() {
+    return (
+      <div className='item-preview-componant-row'>
+          <hr/>
+          <Row className='item-preview-componant-row'>
+            <Col span={4} offset={1}>
+                <Image style={{width:80}} src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"/>
+            </Col>
+            <Col span={10} offset={1}>
+                <Text strong>Item Name</Text><br/>
+                <Text type="secondary" strong>125.33 $</Text><br/>
+                <Text type="secondary" strong>Quantity : 5</Text>
+                
+            </Col>
+          </Row>
+      </div>
+    )
+  }
+}
---
+++ e_commerce_fe/src/pages/OrderSummaryDetails.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Row, Col, Image, Typography  } from 'antd';
+const { Text,Title } = Typography;
+
+export default class OrderSummaryDetails extends Component {
+  render() {
+    return (
+      <div>
+        <Row className='order-summary-row'>
+            <Col span={10} offset = {2}>
+                <Text strong type="secondary">Sub Total</Text>
+            </Col>
+            <Col span={6} offset = {4} className='order-summary-right'>
+                <Text strong type="secondary">15.55 $</Text>
+            </Col>
+        </Row>
+        <Row className='order-summary-row'>
+            <Col span={10} offset = {2}>
+                <Text strong type="secondary">Shipping Cost</Text>
+            </Col>
+            <Col span={6} offset = {4} className='order-summary-right'>
+                <Text strong type="secondary">0.00 $</Text>
+            </Col>
+        </Row>
+        <Row className='order-summary-row'>
+            <Col span={10} offset = {2}>
+                <Text strong type="secondary">Discounts</Text>
+            </Col>
+            <Col span={6} offset = {4} className='order-summary-right'>
+                <Text strong type="secondary">0.00 $</Text>
+            </Col>
+        </Row>
+        <Row className='order-summary-row'>
+            <Col span={10} offset = {2}>
+                <Title strong level={4}>Total</Title>
+            </Col>
+            <Col span={6} offset = {4} className='order-summary-right'>
+                <Title strong level={4}>15.55 $</Title>
+            </Col>
+        </Row>
+      </div>
+    )
+  }
+}
