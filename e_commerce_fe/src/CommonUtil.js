37264a0e627807d2ffdda9f128454f6498951731
MahendraThammita <dhmmpthammita@gmail.com>
2022-05-17 23:59:03 +0530

    Update CommonUtil.js

--- e_commerce_fe/src/CommonUtil.js
+++ e_commerce_fe/src/CommonUtil.js
@@ -1,5 +1,5 @@
 const baseUrls = {
-    paymentBaseURL : 'http://localhost:8080',
+    paymentBaseURL : 'http://20.210.57.143:8080',
     userBaseURL : 'http://20.89.164.178:3000',
     itemBaseURL : 'http://20.78.251.90:5000',
     cartBaseURL : 'http://20.188.18.253:8070/cart'
